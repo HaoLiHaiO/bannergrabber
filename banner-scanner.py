c1c81d02817d31f450f51b2c7f9e54b1f92a261b
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-11 10:34:13 +0000

    use functions

---
+++ banner-scanner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import socket
+
+def banner(ip, port):
+    s = socket.socket()
+    s.connect((ip, int(port)))
+    print(s.recv(1024))
+
+def main():
+    ip = input("Please enter an IP: ")
+    port = str(input("Please enter a port: "))
+    banner(ip, port)
+
+main()
